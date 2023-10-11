10ebf1772e021c2d20630c00c13e76446875c863
dalbr96 <danielburgosr@outlook.com>
2023-10-11 15:30:57 -0500

    ADD - navbar main component: maquetación inicial

--- todo-app-fr/src/app/core/modules/layout/main/main.component.ts
+++ todo-app-fr/src/app/core/modules/layout/main/main.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { menuAnimation, navbarAnimation } from './main.animation';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
+  animations: [menuAnimation, navbarAnimation]
 })
 export class MainComponent {
+
+  openMenu = false;
+
+  toogleOpenMenu() {
+    this.openMenu = !this.openMenu;
+  }
 
 }

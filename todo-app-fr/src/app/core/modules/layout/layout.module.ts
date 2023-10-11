e3c92936d71453e80bc13ab9015171f6007454a2
dalbr96 <danielburgosr@outlook.com>
2023-10-11 08:18:07 -0500

    ADD - Maquetacion footer

--- todo-app-fr/src/app/core/modules/layout/layout.module.ts
+++ todo-app-fr/src/app/core/modules/layout/layout.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     LayoutComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule

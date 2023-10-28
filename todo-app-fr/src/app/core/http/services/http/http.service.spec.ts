19b56f7af5f6d1626c9c012e1047df001940bc29
dalbr96 <danielburgosr@outlook.com>
2023-10-28 09:15:03 -0500

    ADD - UnitTest HttpService

--- todo-app-fr/src/app/core/http/services/http/http.service.spec.ts
+++ todo-app-fr/src/app/core/http/services/http/http.service.spec.ts
@@ -3,10 +3,12 @@ import { TestBed } from '@angular/core/testing';
 import { HttpService } from './http.service';
 import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { environment } from 'src/environments/environment';
 
 
 describe('HttpService', () => {
   let service: HttpService;
+  let httpClient: HttpClient;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -14,9 +16,26 @@ describe('HttpService', () => {
       providers: [HttpClient]
     });
     service = TestBed.inject(HttpService);
+    httpClient = TestBed.inject(HttpClient);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should invoke the get method of httpClient', () => {
+    spyOn(httpClient, 'get');
+
+    service.get('/test');
+
+    expect(httpClient.get).toHaveBeenCalledWith(`${environment.urlBase}${environment.api}/test`, { headers: undefined });
+  });
+
+  it('should invoke the post method of httpClient', () => {
+    spyOn(httpClient, 'post');
+
+    service.post('/test', {});
+
+    expect(httpClient.post).toHaveBeenCalledWith(`${environment.urlBase}${environment.api}/test`, {});
   });
 });
